bad3dbb20b3dcd2586a963d5f72c8d20e6cb083b
matsumoto naoma <naoma.matsumoto@gmail.com>
2021-03-26 13:48:08 +0900

    update layout

--- app/src/main/java/com/example/myapplicationtest3/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtest3/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.myapplicationtest3
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
+import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
@@ -46,15 +49,40 @@ class MainActivity : AppCompatActivity() {
         //④LineDataにLineDataSet格納
         val lineData = LineData(lineDataSets)
         //⑤LineChartにLineData格納
-        val lineChart: com.github.mikephil.charting.charts.LineChart = findViewById(R.id.lineChartExample)
+        val lineChart: LineChart = findViewById(R.id.lineChartExample)
         lineChart.data = lineData
         //⑥Chartのフォーマット指定(3章で詳説)
         //X軸の設定
         lineChart.xAxis.apply {
-            val isEnabled = true
-            val textColor = Color.BLACK
+            isEnabled = true
+            textColor = Color.BLACK
         }
+        //左Y軸の設定 → フォーマット指定処理
+//        lineChart.axisLeft.apply {
+//            isEnabled = true
+//            textColor = Color.BLACK
+//            axisMinValue = -3.0f
+//        }
+        println(lineChart)
+
+
         //⑦linechart更新
         lineChart.invalidate()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when(item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
