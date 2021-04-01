62937e1aafde847f915457720cd298a33f124c2d
matsumoto naoma <naoma.matsumoto@gmail.com>
2021-04-01 15:43:41 +0900

    only with home grafh

--- app/src/main/java/com/example/myapplication_test/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_test/MainActivity.kt
@@ -36,59 +36,10 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-
-
         // 別クラスに呼び出せる
         val apiUrl = "http://mattun-pattun.com/api/delete";
         val result = HitAPITask().execute(apiUrl, "DELETE")
         println(result)
-
-
-        //表示用サンプルデータの作成//
-        val x = listOf<Float>(1f, 2f, 3f, 5f, 8f, 13f, 21f, 34f)//X軸データ
-        val y = x.map{it*it}//Y軸データ（X軸の2乗）
-
-        //①Entryにデータ格納
-        var entryList = mutableListOf<Entry>()//1本目の線
-        for(i in x.indices){
-            entryList.add(
-                Entry(x[i], y[i])
-            )
-        }
-
-        //LineDataSetのList
-        val lineDataSets = mutableListOf<ILineDataSet>()
-        //②DataSetにデータ格納
-        val lineDataSet = LineDataSet(entryList as List<Entry>?, "square")
-        //③DataSetにフォーマット指定(3章で詳説)
-        lineDataSet.color = Color.BLUE
-        //リストに格納
-        lineDataSets.add(lineDataSet)
-
-        //④LineDataにLineDataSet格納
-        val lineData = LineData(lineDataSets)
-        //⑤LineChartにLineData格納
-        val lineChart: LineChart = findViewById(R.id.lineChartExample)
-        lineChart.data = lineData
-        //⑥Chartのフォーマット指定(3章で詳説)
-        //X軸の設定
-        lineChart.xAxis.apply {
-            isEnabled = true
-            textColor = Color.BLACK
-        }
-
-        //左Y軸の設定 → フォーマット指定処理
-//        lineChart.axisLeft.apply {
-//            isEnabled = true
-//            textColor = Color.BLACK
-//            axisMinValue = -3.0f
-//        }
-        println(lineChart)
-
-
-        //⑦linechart更新
-        lineChart.invalidate()
-
 
     }
 }
--- app/src/main/java/com/example/myapplication_test/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication_test/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication_test.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication_test.R
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
 class HomeFragment : Fragment() {
 
@@ -22,10 +28,56 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+
+        //表示用サンプルデータの作成//
+        val x = listOf<Float>(1f, 2f, 3f, 5f, 8f, 13f, 21f, 34f)//X軸データ
+        val y = x.map{it*it}//Y軸データ（X軸の2乗）
+
+        //①Entryにデータ格納
+        var entryList = mutableListOf<Entry>()//1本目の線
+        for(i in x.indices){
+            entryList.add(
+                Entry(x[i], y[i])
+            )
+        }
+
+        //LineDataSetのList
+        val lineDataSets = mutableListOf<ILineDataSet>()
+        //②DataSetにデータ格納
+        val lineDataSet = LineDataSet(entryList as List<Entry>?, "square")
+        //③DataSetにフォーマット指定(3章で詳説)
+        lineDataSet.color = Color.BLUE
+        //リストに格納
+        lineDataSets.add(lineDataSet)
+
+        //④LineDataにLineDataSet格納
+        val lineData = LineData(lineDataSets)
+        //⑤LineChartにLineData格納
+        val lineChart: LineChart = root.findViewById(R.id.lineChartExample)
+        lineChart.data = lineData
+        //⑥Chartのフォーマット指定(3章で詳説)
+        //X軸の設定
+        lineChart.xAxis.apply {
+            isEnabled = true
+            textColor = Color.BLACK
+        }
+
+        //左Y軸の設定 → フォーマット指定処理
+//        lineChart.axisLeft.apply {
+//            isEnabled = true
+//            textColor = Color.BLACK
+//            axisMinValue = -3.0f
+//        }
+        println(lineChart)
+
+
+        //⑦linechart更新
+        lineChart.invalidate()
+
         return root
     }
 }
